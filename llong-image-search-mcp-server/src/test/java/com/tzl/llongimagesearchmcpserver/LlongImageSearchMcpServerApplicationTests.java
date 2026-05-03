3da2e279579b59d4668ec377106f3e9e8393d75d
tzl <2773604953@qq.com>
2026-05-03 08:44:59 +0800

    7.1 和 7.2(补充)

---
+++ llong-image-search-mcp-server/src/test/java/com/tzl/llongimagesearchmcpserver/LlongImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzl.llongimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlongImageSearchMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
